8ad83d7f5a65f2c544237c7ab35c3eb55d8dba27
karamyakoub <karamyakoubagha>
2020-06-15 10:27:29 -0300

    15/06/2020 beta version finalized

--- app/src/main/java/com/karam/transport/MainActivity.java
+++ app/src/main/java/com/karam/transport/MainActivity.java
@@ -5,23 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import java.util.List;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity{
-    private static final long SPLASH_SCREEN_MS = 1500;
-    long gapTime;
-    private long mTimeBeforeDelay;
-    private Handler mSplashHandler;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     AlertDialog alertDialog;
 
     @Override
@@ -29,19 +28,32 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final String TAG = getString(R.string.main_activity_tag);
-        //mSplashHandler = new Handler();
-        Dexter.withContext(this).withPermissions(Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                .withListener(new MultiplePermissionsListener() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
                     @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
                         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                         startActivity(intent);
                         finish();
                     }
+
                     @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        View view = Methods.setToastView(MainActivity.this,getString(R.string.permission_title),true,
+                                getString(R.string.permission_location), true,"Conceder",true,
+                                "Negar",true);
+                        Button btnConfirm = view.findViewById(R.id.toast_btn_confirm);
+                        btnConfirm.setOnClickListener(MainActivity.this);
+                        Button btnCancel= view.findViewById(R.id.toast_btn_dismiss);
+                        btnCancel.setOnClickListener(MainActivity.this);
+                        alertDialog = new AlertDialog.Builder(MainActivity.this)
+                                .setView(view).create();
+                        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        alertDialog.show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
                         permissionToken.continuePermissionRequest();
                     }
                 }).check();
@@ -56,4 +68,19 @@ public class MainActivity extends AppCompatActivity{
         super.onPause();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toast_btn_confirm:
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getApplicationContext().getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+                break;
+            case R.id.toast_btn_dismiss:
+                alertDialog.cancel();
+                break;
+        }
+    }
 }
